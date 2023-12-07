b0813112f6d34be6e55a0ca1dcabf0d8fc666366
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2023-12-07 21:51:05 +0700

    правки маркеров

--- src/routes/item.routes.js
+++ src/routes/item.routes.js
@@ -5,57 +5,91 @@ import { ImageUrl } from "./ImageUrl.js";
 const ItemRouter = Router();
 
 ItemRouter.get("/item", async (req, res) => {
-    
-    const data = await Item.find();
-    res.json(data);
-})
+  const data = await Item.find();
+  res.json(data);
+});
 
 ItemRouter.post("/item", async (req, res) => {
-    const {name, description, price, oldPrice, image, count, category, brand, marcers} = req.body;
-    const data = await Item.create(
-        {
-            name, description, price, oldPrice, count, category, brand, image: ImageUrl + image,
-            marcers: {
-                no: marcers.no ?? false,
-                new: marcers.new ?? false,
-                promotion: marcers.promotion ?? false,
-                discount: marcers.discount ?? false,
-                hot: marcers.hot ?? false,
-            }
-        }
-    );
-    res.json(data);
-})
-
+  const {
+    name,
+    description,
+    price,
+    oldPrice,
+    image,
+    count,
+    category,
+    brand,
+    marcers,
+  } = req.body;
+  const data = await Item.create({
+    name,
+    description,
+    price,
+    oldPrice,
+    count,
+    category,
+    brand,
+    image: ImageUrl + image,
+    marcers: {
+      no: count === 0 || false,
+      new: marcers.new ?? false,
+      promotion: marcers.promotion ?? false,
+      discount: oldPrice !== 0 || false,
+      hot: marcers.hot ?? false,
+    },
+  });
+  res.json(data);
+});
 
 ItemRouter.delete("/item", async (req, res) => {
-    const data = await Item.findOneAndDelete().where(
-        {
-            _id: req.body._id
-        }
-    );
-    FileController.deleteFile(data.image);
-    res.json(data);
-})
+  const data = await Item.findOneAndDelete().where({
+    _id: req.body._id,
+  });
+  FileController.deleteFile(data.image);
+  res.json(data);
+});
 
 ItemRouter.put("/item", async (req, res) => {
-    const {_id, name, description, price, oldPrice, image, count, category, brand, marcers} = req.body;
+  const {
+    _id,
+    name,
+    description,
+    price,
+    oldPrice,
+    image,
+    count,
+    category,
+    brand,
+    marcers,
+  } = req.body;
 
-    const oldData = await Item.findOneAndDelete(
-        {
-            _id: _id
+  const oldData = await Item.findOneAndUpdate(
+    {
+      _id: _id,
+    },
+    {
+      $set: {
+        name,
+        description,
+        price,
+        oldPrice,
+        image: ImageUrl + image,
+        count,
+        category,
+        brand,
+        marcers: {
+          no: count === 0 || false,
+          new: marcers.new ?? false,
+          promotion: marcers.promotion ?? false,
+          discount: oldPrice !== 0 || false,
+          hot: marcers.hot ?? false,
         },
-        {$set: {
-            name, description, price, oldPrice, image: ImageUrl + image, count, category, brand, marcers
-        }}
-    );
-    const data = await Item.findOne({_id: req.body._id});
-    if (data.image !== oldData.image) FileController.deleteFile(oldData.image);
-    res.json(data);
-})
-
-
-
-
+      },
+    }
+  );
+  const data = await Item.findOne({ _id: req.body._id });
+  if (data.image !== oldData.image) FileController.deleteFile(oldData.image);
+  res.json(data);
+});
 
 export default ItemRouter;
