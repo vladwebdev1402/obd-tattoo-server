608f3d8f8f1daab0f17c34c8e96fd6a06552dba8
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2023-12-13 00:32:56 +0700

    операции с корзиной

--- src/routes/databaseRoutes/basket.routes.js
+++ src/routes/databaseRoutes/basket.routes.js
@@ -6,41 +6,61 @@ const BasketRouter = Router();
 
 BasketRouter.get("/basket", AuthMiddleware(["DATABASE_ADMIN", "CLIENT"], []), async (req, res) => {
   const data = await Client.find({
-    _id: req.query.id,
-  }).select("basket");
-  return res.json({data, message: "Корзина получена"});
+    _id: req.user_id,
+  }).select(["basket", "-_id"]);
+  return res.json({data: data[0].basket, message: "Корзина получена"});
 });
 
 BasketRouter.post("/basket", AuthMiddleware(["DATABASE_ADMIN", "CLIENT"], {}), async (req, res) => {
-  const { client, item, count } = req.body;
-  const shopItem = await Item.find({_id: item}).then(res => res[0]);
-  if (count > 0 && shopItem !== undefined && count <= shopItem.count) {
-    const data = await Client.findOneAndUpdate(
-        {
-          _id: client,
-        },
-        {
-          $push: {
-            basket: {
-              item,
-              count,
-            },
-          }
-        }
-      );
-      return res.json({data, message: 'Товары добавлены'});
+  const { item, count } = req.body;
+  const shopItem = (await Item.find({_id: item}))[0];
+  const basket = (await Client.find({_id: req.user_id}))[0].basket;
+  const itemInBasket = basket.filter(basketItem => basketItem.item == shopItem._id.toString())[0];
+
+  // если в корзине есть товар и меняется количество товара
+  if (count > 0 && shopItem !== undefined && itemInBasket !== undefined && count <= shopItem.count) {
+    const newBasket = basket.map(basketItem => {
+      if (basketItem.item.toString() == item) return {item, count};
+      return basketItem;
+    })
+
+    await Client.updateOne({
+      _id: req.user_id,
+    }, {
+      $set: {
+        basket: newBasket
+      }
+    })
+
+    return res.json({data: newBasket, message: 'Товары добавлены'});
   }
-  else if (count == 0) {
-    const data = await Client.updateOne({
-        _id: client
-    }, {$pull: {
-        basket: {
-            item
-        }
-    }})
-    return res.json({data, message: 'Товары удалён'});
+
+  //если в корзине нет товара
+  else if (shopItem !== undefined && count !== 0 && itemInBasket === undefined) {
+    const newBasket = [...basket, {item, count}];
+    await Client.updateOne({
+      _id: req.user_id,
+    }, {
+      $set: {
+        basket: newBasket
+      }
+    })
+    return res.json({data: newBasket, message: 'В корзину добавлены новые товары'});
   }
-  else return res.json({data: {}, message: "Такое количество товаров отсутствует на складе"});
+
+  // если хотим удалить товар
+  else if (count <= 0) {
+    const newBasket = basket.filter(basketItem => basketItem.item.toString() != item);
+    await Client.updateOne({
+      _id: req.user_id,
+    }, {
+      $set: {
+        basket: newBasket
+      }
+    })
+    return res.json({data: newBasket, message: 'Товар удалён'});
+  }
+  else return res.json({data: basket, message: "Такое количество товаров отсутствует на складе"});
 });
 
 export default BasketRouter;
--- src/routes/databaseRoutes/client.routes.js
+++ src/routes/databaseRoutes/client.routes.js
@@ -3,9 +3,10 @@ import Client from "../../model/ClientModel.js";
 import { AuthMiddleware } from "../../middleware/AuthMiddleware.js";
 const ClientRouter = Router();
 
-ClientRouter.get("/client", AuthMiddleware(["DATABASE_ADMIN"], []), async (req, res) => {
-    const data = await Client.find();
-    return res.json({data, message: "Пользователи успешно получены"});
+ClientRouter.get("/client", AuthMiddleware(["DATABASE_ADMIN", "CLIENT"], []), async (req, res) => {
+
+    const data = await Client.find({_id: req.user_id}, {login: 0, password: 0, role: 0});
+    return res.json({data: data[0], message: "Пользователи успешно получены"});
 })
 
 ClientRouter.post("/client", AuthMiddleware(["DATABASE_ADMIN"], {}), async (req, res) => {
@@ -30,15 +31,16 @@ ClientRouter.delete("/client", AuthMiddleware(["DATABASE_ADMIN"], {}), async (re
 })
 
 ClientRouter.put("/client", AuthMiddleware(["DATABASE_ADMIN", "CLIENT"], {}), async (req, res) => {
-    const {_id, name, surname, patronname, phone, mail, 
-        apartament, entrance, floo, intercom, city, street} = req.body
-    const data = await Client.updateOne(
+    const {_id, name, surname, patroname, phone, mail, 
+        apartament, entrance, floor, intercom, city, street} = req.body
+    const data = await Client.findOneAndUpdate(
         {
-            _id: _id
+            _id: _id ?? req.user_id,
+          
         },
         {$set: {
-            name, surname, patronname, phone, mail, 
-        apartament, entrance, floo, intercom, city, street
+            name, surname, patroname, phone, mail, 
+        apartament, entrance, floor, intercom, city, street
         }}
     );
     return res.json({data, message: "Личный кабинет успешно отредактирован"});
